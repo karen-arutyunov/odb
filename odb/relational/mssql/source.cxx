7d9b3fb7a083fa1ac09b1c1755951c713db04f50
Boris Kolpackov <boris@codesynthesis.com>
2012-01-08 17:32:52 +0200

    Add support for defining composite value type as class template instantiations (mssql)

--- odb/relational/mssql/source.cxx
+++ odb/relational/mssql/source.cxx
@@ -504,7 +504,7 @@ namespace relational
               // Handle NULL pointers and extract the id.
               //
               os << "{"
-                 << "typedef object_traits< " << c->fq_name () <<
+                 << "typedef object_traits< " << class_fq_name (*c) <<
                 " > obj_traits;";
 
               if (weak_pointer (mt))
@@ -876,7 +876,7 @@ namespace relational
               // Handle NULL pointers and extract the id.
               //
               os << "{"
-                 << "typedef object_traits< " << c->fq_name () <<
+                 << "typedef object_traits< " << class_fq_name (*c) <<
                 " > obj_traits;"
                  << "typedef pointer_traits< " << mi.fq_type () <<
                 " > ptr_traits;"
