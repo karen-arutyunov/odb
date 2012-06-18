47d783b340df91a43abc36470c78f998db0b33fe
Boris Kolpackov <boris@codesynthesis.com>
2012-06-18 09:34:39 +0200

    Bump version to 2.0.1

--- odb/sqlite/version.hxx
+++ odb/sqlite/version.hxx
@@ -35,8 +35,8 @@
 // libodb-sqlite version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_SQLITE_VERSION     2000000
-#define LIBODB_SQLITE_VERSION_STR "2.0.0"
+#define LIBODB_SQLITE_VERSION     2000100
+#define LIBODB_SQLITE_VERSION_STR "2.0.1"
 
 #include <odb/post.hxx>
 
