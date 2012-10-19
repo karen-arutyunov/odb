drok/libodb-mysql

Completion of prepared query support

odb/mysql/connection.hxx
@@ -15,6 +15,7 @@
 #include <odb/mysql/mysql.hxx>
 #include <odb/mysql/version.hxx>
 #include <odb/mysql/forward.hxx>
+#include <odb/mysql/query.hxx>
 #include <odb/mysql/tracer.hxx>
 #include <odb/mysql/transaction-impl.hxx>
 #include <odb/mysql/auto-handle.hxx>
@@ -60,6 +61,21 @@ namespace odb
 
       virtual unsigned long long
       execute (const char* statement, std::size_t length);
+
+      // Query preparation.
+      //
+    public:
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const char*);
+
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const std::string&);
+
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const query<T>&);
 
       // SQL statement tracing.
       //
@@ -182,6 +198,8 @@ namespace odb
     };
   }
 }
+
+#include <odb/mysql/connection.ixx>
 
 #include <odb/post.hxx>
 

odb/mysql/connection.ixx
@@ -1,0 +1,30 @@
+// file      : odb/mysql/connection.ixx
+// copyright : Copyright (c) 2005-2012 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+namespace odb
+{
+  namespace mysql
+  {
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const char* q)
+    {
+      return prepare_query<T> (n, query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const std::string& q)
+    {
+      return prepare_query<T> (n, query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const query<T>& q)
+    {
+      return query_<T, id_mysql>::call (*this, n, q);
+    }
+  }
+}

odb/mysql/database.ixx
@@ -2,6 +2,8 @@
 // copyright : Copyright (c) 2009-2012 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
+#include <odb/mysql/transaction.hxx>
+
 namespace odb
 {
   namespace mysql
@@ -397,6 +399,30 @@ namespace odb
         r.cache ();
 
       return r;
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const char* q)
+    {
+      return prepare_query<T> (n, mysql::query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const std::string& q)
+    {
+      return prepare_query<T> (n, mysql::query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const mysql::query<T>& q)
+    {
+      // Throws if not in transaction.
+      //
+      mysql::connection& c (transaction::current ().connection ());
+      return c.prepare_query (n, q);
     }
   }
 }

karen-arutyunov/odb

odb/sqlite/connection.hxx
@@ -19,6 +19,7 @@
 
 #include <odb/sqlite/version.hxx>
 #include <odb/sqlite/forward.hxx>
+#include <odb/sqlite/query.hxx>
 #include <odb/sqlite/tracer.hxx>
 #include <odb/sqlite/transaction-impl.hxx>
 #include <odb/sqlite/auto-handle.hxx>
@@ -67,6 +68,21 @@ namespace odb
 
       virtual unsigned long long
       execute (const char* statement, std::size_t length);
+
+      // Query preparation.
+      //
+    public:
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const char*);
+
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const std::string&);
+
+      template <typename T>
+      prepared_query<T>
+      prepare_query (const char* name, const query<T>&);
 
       // SQL statement tracing.
       //
@@ -152,6 +168,8 @@ namespace odb
     };
   }
 }
+
+#include <odb/sqlite/connection.ixx>
 
 #include <odb/post.hxx>
 

odb/sqlite/connection.ixx
@@ -1,0 +1,30 @@
+// file      : odb/sqlite/connection.ixx
+// copyright : Copyright (c) 2005-2012 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+namespace odb
+{
+  namespace sqlite
+  {
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const char* q)
+    {
+      return prepare_query<T> (n, query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const std::string& q)
+    {
+      return prepare_query<T> (n, query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> connection::
+    prepare_query (const char* n, const query<T>& q)
+    {
+      return query_<T, id_sqlite>::call (*this, n, q);
+    }
+  }
+}

odb/sqlite/database.ixx
@@ -2,6 +2,8 @@
 // copyright : Copyright (c) 2009-2012 Code Synthesis Tools CC
 // license   : GNU GPL v2; see accompanying LICENSE file
 
+#include <odb/sqlite/transaction.hxx>
+
 namespace odb
 {
   namespace sqlite
@@ -392,6 +394,30 @@ namespace odb
       // here; object_traits::query () does this.
       //
       return query_<T, id_sqlite>::call (*this, q);
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const char* q)
+    {
+      return prepare_query<T> (n, sqlite::query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const std::string& q)
+    {
+      return prepare_query<T> (n, sqlite::query<T> (q));
+    }
+
+    template <typename T>
+    inline prepared_query<T> database::
+    prepare_query (const char* n, const sqlite::query<T>& q)
+    {
+      // Throws if not in transaction.
+      //
+      sqlite::connection& c (transaction::current ().connection ());
+      return c.prepare_query (n, q);
     }
   }
 }