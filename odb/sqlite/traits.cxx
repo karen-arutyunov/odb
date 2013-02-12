b84408a833511bb03b8dbe031ec06d6d09ca5bc5
Boris Kolpackov <boris@codesynthesis.com>
2013-02-12 13:48:12 +0200

    Qualify vector to work around Sun CC bugs

--- odb/sqlite/traits.cxx
+++ odb/sqlite/traits.cxx
@@ -174,7 +174,9 @@ namespace odb
     //
     // default_value_traits<vector<char>, id_blob>
     //
-    void default_value_traits<vector<char>, id_blob>::
+    // std::vector has to be qualified for Sun CC.
+    //
+    void default_value_traits<std::vector<char>, id_blob>::
     set_image (details::buffer& b,
                size_t& n,
                bool& is_null,
@@ -195,7 +197,9 @@ namespace odb
     //
     // default_value_traits<vector<unsigned char>, id_blob>
     //
-    void default_value_traits<vector<unsigned char>, id_blob>::
+    // std::vector has to be qualified for Sun CC.
+    //
+    void default_value_traits<std::vector<unsigned char>, id_blob>::
     set_image (details::buffer& b,
                size_t& n,
                bool& is_null,
