4b4cea7da1fbfd0ea90124932450fbde53a84c2e
Boris Kolpackov <boris@codesynthesis.com>
2013-02-12 16:55:50 +0200

    Factor windows.h inclusion into separate header, disable min/max macros

--- odb/sqlite/database.cxx
+++ odb/sqlite/database.cxx
@@ -3,10 +3,7 @@
 // license   : GNU GPL v2; see accompanying LICENSE file
 
 #ifdef _WIN32
-#  ifndef WIN32_LEAN_AND_MEAN
-#    define WIN32_LEAN_AND_MEAN
-#  endif
-#  include <windows.h> // WideCharToMultiByte
+#  include <odb/details/win32/windows.hxx> // WideCharToMultiByte
 #endif
 
 #include <sstream>
