drok/libodb-mysql

Schema versioning support

odb/mysql/no-id-object-result.txx
@@ -36,10 +36,12 @@ namespace odb
     no_id_object_result_impl<T>::
     no_id_object_result_impl (const query_base&,
                               details::shared_ptr<select_statement> statement,
-                              statements_type& statements)
+                              statements_type& statements,
+                              const schema_version_migration* svm)
         : base_type (statements.connection ()),
           statement_ (statement),
           statements_ (statements),
+          tc_ (svm),
           count_ (0)
     {
     }
@@ -52,7 +54,7 @@ namespace odb
         fetch ();
 
       object_traits::callback (this->db_, obj, callback_event::pre_load);
-      object_traits::init (obj, statements_.image (), &this->db_);
+      tc_.init (obj, statements_.image (), &this->db_);
       object_traits::callback (this->db_, obj, callback_event::post_load);
     }
 
@@ -95,7 +97,7 @@ namespace odb
         if (im.version != statements_.select_image_version ())
         {
           binding& b (statements_.select_image_binding ());
-          object_traits::bind (b.bind, im, statement_select);
+          tc_.bind (b.bind, im, statement_select);
           statements_.select_image_version (im.version);
           b.version++;
         }
@@ -116,14 +118,13 @@ namespace odb
 
             typename object_traits::image_type& im (statements_.image ());
 
-            if (object_traits::grow (
-                  im, statements_.select_image_truncated ()))
+            if (tc_.grow (im, statements_.select_image_truncated ()))
               im.version++;
 
             if (im.version != statements_.select_image_version ())
             {
               binding& b (statements_.select_image_binding ());
-              object_traits::bind (b.bind, im, statement_select);
+              tc_.bind (b.bind, im, statement_select);
               statements_.select_image_version (im.version);
               b.version++;
               statement_->refetch ();

odb/mysql/simple-object-result.hxx
@@ -9,6 +9,7 @@
 
 #include <cstddef> // std::size_t
 
+#include <odb/schema-version.hxx>
 #include <odb/simple-object-result.hxx>
 
 #include <odb/details/shared-ptr.hxx>
@@ -16,6 +17,7 @@
 #include <odb/mysql/version.hxx>
 #include <odb/mysql/forward.hxx> // query_base
 #include <odb/mysql/statement.hxx>
+#include <odb/mysql/traits-calls.hxx>
 
 namespace odb
 {
@@ -41,7 +43,8 @@ namespace odb
 
       object_result_impl (const query_base&,
                           details::shared_ptr<select_statement>,
-                          statements_type&);
+                          statements_type&,
+                          const schema_version_migration*);
 
       virtual void
       load (object_type&, bool fetch);
@@ -70,6 +73,7 @@ namespace odb
     private:
       details::shared_ptr<select_statement> statement_;
       statements_type& statements_;
+      object_traits_calls<object_type> tc_;
       std::size_t count_;
     };
   }

odb/mysql/view-result.hxx
@@ -9,6 +9,7 @@
 
 #include <cstddef> // std::size_t
 
+#include <odb/schema-version.hxx>
 #include <odb/view-result.hxx>
 
 #include <odb/details/shared-ptr.hxx>
@@ -16,6 +17,7 @@
 #include <odb/mysql/version.hxx>
 #include <odb/mysql/forward.hxx> // query_base, view_statements
 #include <odb/mysql/statement.hxx>
+#include <odb/mysql/traits-calls.hxx>
 
 namespace odb
 {
@@ -40,7 +42,8 @@ namespace odb
 
       view_result_impl (const query_base&,
                         details::shared_ptr<select_statement>,
-                        statements_type&);
+                        statements_type&,
+                        const schema_version_migration*);
 
       virtual void
       load (view_type&);
@@ -66,6 +69,7 @@ namespace odb
     private:
       details::shared_ptr<select_statement> statement_;
       statements_type& statements_;
+      view_traits_calls<view_type> tc_;
       std::size_t count_;
     };
   }

karen-arutyunov/odb

odb/sqlite/no-id-object-result.txx
@@ -37,11 +37,13 @@ namespace odb
     no_id_object_result_impl<T>::
     no_id_object_result_impl (const query_base& q,
                               const details::shared_ptr<select_statement>& s,
-                              statements_type& sts)
+                              statements_type& sts,
+                              const schema_version_migration* svm)
         : base_type (sts.connection ()),
           params_ (q.parameters ()),
           statement_ (s),
-          statements_ (sts)
+          statements_ (sts),
+          tc_ (svm)
     {
     }
 
@@ -57,7 +59,7 @@ namespace odb
       if (im.version != statements_.select_image_version ())
       {
         binding& b (statements_.select_image_binding ());
-        object_traits::bind (b.bind, im, statement_select);
+        tc_.bind (b.bind, im, statement_select);
         statements_.select_image_version (im.version);
         b.version++;
       }
@@ -66,13 +68,13 @@ namespace odb
 
       if (r == select_statement::truncated)
       {
-        if (object_traits::grow (im, statements_.select_image_truncated ()))
+        if (tc_.grow (im, statements_.select_image_truncated ()))
           im.version++;
 
         if (im.version != statements_.select_image_version ())
         {
           binding& b (statements_.select_image_binding ());
-          object_traits::bind (b.bind, im, statement_select);
+          tc_.bind (b.bind, im, statement_select);
           statements_.select_image_version (im.version);
           b.version++;
           statement_->reload ();
@@ -80,7 +82,7 @@ namespace odb
       }
 
       object_traits::callback (this->db_, obj, callback_event::pre_load);
-      object_traits::init (obj, im, &this->db_);
+      tc_.init (obj, im, &this->db_);
       object_traits::callback (this->db_, obj, callback_event::post_load);
     }
 

odb/sqlite/simple-object-result.hxx
@@ -9,6 +9,7 @@
 
 #include <cstddef> // std::size_t
 
+#include <odb/schema-version.hxx>
 #include <odb/simple-object-result.hxx>
 
 #include <odb/details/shared-ptr.hxx>
@@ -16,6 +17,7 @@
 #include <odb/sqlite/version.hxx>
 #include <odb/sqlite/forward.hxx> // query_base, query_params
 #include <odb/sqlite/statement.hxx>
+#include <odb/sqlite/traits-calls.hxx>
 
 namespace odb
 {
@@ -41,7 +43,8 @@ namespace odb
 
       object_result_impl (const query_base&,
                           const details::shared_ptr<select_statement>&,
-                          statements_type&);
+                          statements_type&,
+                          const schema_version_migration*);
 
       virtual void
       load (object_type&, bool fetch);
@@ -74,6 +77,7 @@ namespace odb
       details::shared_ptr<query_params> params_;
       details::shared_ptr<select_statement> statement_;
       statements_type& statements_;
+      object_traits_calls<object_type> tc_;
     };
   }
 }

odb/sqlite/view-result.hxx
@@ -9,6 +9,7 @@
 
 #include <cstddef> // std::size_t
 
+#include <odb/schema-version.hxx>
 #include <odb/view-result.hxx>
 
 #include <odb/details/shared-ptr.hxx>
@@ -16,6 +17,7 @@
 #include <odb/sqlite/version.hxx>
 #include <odb/sqlite/forward.hxx> // query_base, query_params
 #include <odb/sqlite/statement.hxx>
+#include <odb/sqlite/traits-calls.hxx>
 
 namespace odb
 {
@@ -40,7 +42,8 @@ namespace odb
 
       view_result_impl (const query_base&,
                         const details::shared_ptr<select_statement>&,
-                        statements_type&);
+                        statements_type&,
+                        const schema_version_migration*);
 
       virtual void
       load (view_type&);
@@ -66,6 +69,7 @@ namespace odb
       details::shared_ptr<query_params> params_;
       details::shared_ptr<select_statement> statement_;
       statements_type& statements_;
+      view_traits_calls<view_type> tc_;
     };
   }
 }