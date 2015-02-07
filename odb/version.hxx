b89634ba6f7533af2fcedac9d7c7259674366387
Boris Kolpackov <boris@codesynthesis.com>
2015-02-07 14:00:39 +0200

    Bump version to 2.4.0

--- odb/version.hxx
+++ odb/version.hxx
@@ -24,12 +24,12 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20306
-#define ODB_VERSION_STR "2.4.a6"
+#define ODB_VERSION     20400
+#define ODB_VERSION_STR "2.4"
 
 // ODB compiler version: interface version plus the bugfix version.
 //
-#define ODB_COMPILER_VERSION     2039906
-#define ODB_COMPILER_VERSION_STR "2.4.0.a6"
+#define ODB_COMPILER_VERSION     2040000
+#define ODB_COMPILER_VERSION_STR "2.4.0"
 
 #endif // ODB_VERSION_HXX
