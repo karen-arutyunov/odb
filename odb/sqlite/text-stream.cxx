857fabab0c537a286e8a39503bf987029669d854
Boris Kolpackov <boris@codesynthesis.com>
2015-09-04 16:48:21 +0200

    Add dummy source files to make Win32 export working

---
+++ odb/sqlite/text-stream.cxx
@@ -0,0 +1,7 @@
+// file      : odb/sqlite/text-stream.cxx
+// copyright : Copyright (c) 2005-2015 Code Synthesis Tools CC
+// license   : GNU GPL v2; see accompanying LICENSE file
+
+#include <odb/sqlite/text-stream.hxx>
+
+// Dummy source file to get Win32 DLL export working.
