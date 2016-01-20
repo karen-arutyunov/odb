9be4e341ac7ca17047cca91a6908c9135506a5f6
Boris Kolpackov <boris@codesynthesis.com>
2016-01-20 18:59:11 +0200

    Bump version to 2.5.0.a4

--- odb/version.hxx
+++ odb/version.hxx
@@ -26,13 +26,13 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20402
-#define ODB_VERSION_STR "2.5.a2"
+#define ODB_VERSION     20404
+#define ODB_VERSION_STR "2.5.a4"
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2049902
-#define LIBODB_VERSION_STR "2.5.0.a2"
+#define LIBODB_VERSION     2049904
+#define LIBODB_VERSION_STR "2.5.0.a4"
 
 #include <odb/post.hxx>
 
