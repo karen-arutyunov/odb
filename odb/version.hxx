9c54bf28fd0f3f2e2f1592c3e8d071ff6decfe2c
Boris Kolpackov <boris@codesynthesis.com>
2017-04-28 16:17:22 +0200

    Bump version to 2.5.0.b.1.z, switch to build2 version module

--- odb/version.hxx
+++ odb/version.hxx
@@ -26,13 +26,13 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20411
-#define ODB_VERSION_STR "2.5-a11"
+#define ODB_VERSION     20451
+#define ODB_VERSION_STR "2.5-b.1"
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2049911
-#define LIBODB_VERSION_STR "2.5.0-a11"
+#define LIBODB_VERSION     2049951
+#define LIBODB_VERSION_STR "2.5.0-b.1"
 
 #include <odb/post.hxx>
 
