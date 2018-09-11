7cec26ca584048d9cb41796c28ee70da5c808364
Boris Kolpackov <boris@codesynthesis.com>
2018-09-11 10:27:53 +0200

    Release version 2.5.0-b.9

--- odb/version.hxx
+++ odb/version.hxx
@@ -30,13 +30,13 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20458
-#define ODB_VERSION_STR "2.5-b.8"
+#define ODB_VERSION     20459
+#define ODB_VERSION_STR "2.5-b.9"
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2049958
-#define LIBODB_VERSION_STR "2.5.0-b.8"
+#define LIBODB_VERSION     2049959
+#define LIBODB_VERSION_STR "2.5.0-b.9"
 
 #include <odb/post.hxx>
 
