29135ad7c28ae928ee5abe17dd4ec18ede855f9a
Boris Kolpackov <boris@codesynthesis.com>
2019-11-21 06:30:31 +0200

    Change version to 2.5.0-b.18.z

--- odb/version.hxx
+++ odb/version.hxx
@@ -24,12 +24,12 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20467
-#define ODB_VERSION_STR "2.5-b.17"
+#define ODB_VERSION     20468
+#define ODB_VERSION_STR "2.5-b.18"
 
 // ODB compiler version: interface version plus the bugfix version.
 //
-#define ODB_COMPILER_VERSION     2049967
-#define ODB_COMPILER_VERSION_STR "2.5.0-b.17"
+#define ODB_COMPILER_VERSION     2049968
+#define ODB_COMPILER_VERSION_STR "2.5.0-b.18"
 
 #endif // ODB_VERSION_HXX
