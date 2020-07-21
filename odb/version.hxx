06b9bef1edf45506570c3924d0e0cd8ff0c644ca
Boris Kolpackov <boris@codesynthesis.com>
2020-07-21 15:13:40 +0200

    Change version to 2.5.0-b.20.z

--- odb/version.hxx
+++ odb/version.hxx
@@ -29,13 +29,13 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20469
-#define ODB_VERSION_STR "2.5-b.19"
+#define ODB_VERSION     20470
+#define ODB_VERSION_STR "2.5-b.20"
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2049969
-#define LIBODB_VERSION_STR "2.5.0-b.19"
+#define LIBODB_VERSION     2049970
+#define LIBODB_VERSION_STR "2.5.0-b.20"
 
 #include <odb/post.hxx>
 
