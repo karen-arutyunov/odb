f8c8491f24cb7d0de03c8ff3defa191f3a2481ba
Boris Kolpackov <boris@codesynthesis.com>
2020-07-21 15:13:40 +0200

    Change version to 2.5.0-b.20.z

--- odb/sqlite/version.hxx
+++ odb/sqlite/version.hxx
@@ -31,15 +31,15 @@
 
 // Check that we have compatible ODB version.
 //
-#if ODB_VERSION != 20469
+#if ODB_VERSION != 20470
 #  error incompatible odb interface version detected
 #endif
 
 // libodb-sqlite version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_SQLITE_VERSION     2049969
-#define LIBODB_SQLITE_VERSION_STR "2.5.0-b.19"
+#define LIBODB_SQLITE_VERSION     2049970
+#define LIBODB_SQLITE_VERSION_STR "2.5.0-b.20"
 
 #include <odb/post.hxx>
 
