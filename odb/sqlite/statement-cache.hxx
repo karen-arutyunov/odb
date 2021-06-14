dfeb43d6ee14be83281945e82ebb484eeb072fe8
Boris Kolpackov <boris@codesynthesis.com>
2021-06-14 11:01:06 +0200

    Work around bogus Clang warning

--- odb/sqlite/statement-cache.hxx
+++ odb/sqlite/statement-cache.hxx
@@ -31,7 +31,7 @@ namespace odb
     public:
       statement_cache (connection& conn)
           : conn_ (conn),
-            version_seq_ (conn.database ().schema_version_sequence ()) {}
+            version_seq_ (conn_.database ().schema_version_sequence ()) {}
 
       template <typename T>
       typename object_traits_impl<T, id_sqlite>::statements_type&
