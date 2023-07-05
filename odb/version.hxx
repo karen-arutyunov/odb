fa7e33f4605c584d3f9b168d06999b7be91c019c
Boris Kolpackov <boris@codesynthesis.com>
2023-07-05 10:10:47 +0200

    Change version to 2.5.0-b.26.z

--- odb/version.hxx
+++ odb/version.hxx
@@ -29,13 +29,13 @@
 
 // ODB interface version: minor, major, and alpha/beta versions.
 //
-#define ODB_VERSION     20475
-#define ODB_VERSION_STR "2.5-b.25"
+#define ODB_VERSION     20476
+#define ODB_VERSION_STR "2.5-b.26"
 
 // libodb version: interface version plus the bugfix version.
 //
-#define LIBODB_VERSION     2049975
-#define LIBODB_VERSION_STR "2.5.0-b.25"
+#define LIBODB_VERSION     2049976
+#define LIBODB_VERSION_STR "2.5.0-b.26"
 
 #include <odb/post.hxx>
 
